d1a280bab805c3404c404129a41316974b9d21fe
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2021-01-11 12:05:14 +0530

    update

--- main.py
+++ main.py
@@ -450,7 +450,7 @@ class Callbacks(object):
                     message = message.replace(f":{emote}:", str(emote_))
 
         for mention in mentions:
-            if mention[2] != "":
+            if mention[2]:
                 member = await guild.query_members(query=mention[2])
                 if member:
                     message = message.replace(mention[1], member[0].mention)

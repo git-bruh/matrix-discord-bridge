b21c82ccd0d1a76e26c8d76434a2b6cff3436898
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2021-05-05 10:30:10 +0530

    fix code injection in queries

--- appservice/db.py
+++ appservice/db.py
@@ -54,29 +54,29 @@ class DataBase(object):
 
         with self.lock:
             self.cur.execute(
-                "INSERT INTO bridge (room_id, channel_id) "
-                f"VALUES ('{room_id}', '{channel_id}')"
+                "INSERT INTO bridge (room_id, channel_id) VALUES (?, ?)",
+                [room_id, channel_id],
             )
             self.conn.commit()
 
     def add_user(self, mxid: str) -> None:
         with self.lock:
-            self.cur.execute(f"INSERT INTO users (mxid) VALUES ('{mxid}')")
+            self.cur.execute("INSERT INTO users (mxid) VALUES (?)", [mxid])
             self.conn.commit()
 
     def add_avatar(self, avatar_url: str, mxid: str) -> None:
         with self.lock:
             self.cur.execute(
-                f"UPDATE users SET avatar_url = '{avatar_url}'"
-                f"WHERE mxid = '{mxid}'"
+                "UPDATE users SET avatar_url = (?) WHERE mxid = (?)",
+                [avatar_url, mxid],
             )
             self.conn.commit()
 
     def add_username(self, username: str, mxid: str) -> None:
         with self.lock:
             self.cur.execute(
-                f"UPDATE users SET username = '{username}'"
-                f"WHERE mxid = '{mxid}'"
+                "UPDATE users SET username = (?) WHERE mxid = (?)",
+                [username, mxid],
             )
             self.conn.commit()
 
